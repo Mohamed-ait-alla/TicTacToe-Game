f5768acdcac1903ec3ed590cb4baff8742c0acc9
Mohamed-ait-alla <mohamedmhdo2000@gmail.com>
2025-08-24 11:46:19 -0400

    add a game mode selection screen at the starting of the game

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [go, setGo] = useState("circle");
   const [isWin, setIsWin] = useState("");
   const [winCases, setWinCases] = useState<number[]>([]);
+  const [mode, setMode] = useState<string>("");
 
   // Hook used for tracking player winning
   useEffect(() => {
@@ -28,18 +29,15 @@ export default function Home() {
       const circleWinsCase = winCase.every((cell) => cells[cell] == "circle");
       const drawCase = (cells.every((cell) => cell !== "") && !isWin);
 
-      if (crossWinsCase)
-      {
+      if (crossWinsCase) {
         setWinCases(winCase);
         setTimeout(() => setIsWin("cross"), 1200);
       }
-      else if (circleWinsCase)
-      {
+      else if (circleWinsCase) {
         setWinCases(winCase);
         setTimeout(() => setIsWin("circle"), 1200);
       }
-      else if (drawCase)
-      {
+      else if (drawCase) {
         setIsWin("draw");
         setWinCases([]);
       }
@@ -69,40 +67,60 @@ export default function Home() {
   return (
     <div className='container'>
       {
-        isWin ? 
-        (
-            <div className="popup">
-              <h2 className={resultClass()}>
-                {isWin === "cross" && "❌ Cross Wins!"}
-                {isWin === "circle" && "⭕️ Circle Wins!"}
-                {isWin === "draw" && "🤝 It's a Draw!"}
-              </h2>
-              <button className="play-again-btn" onClick={handlePlayAgain}>Play Again</button>
+        !mode ?
+          (
+            <div className="mode-container">
+              <h1 className="mode-welcome">Welcome to Tic-Tac-Toe 🎮</h1>
+              <p className="mode-chose-title">Chose Your Game Mode</p>
+              <div className="mode-buttons">
+                <button className="mode-btn friend" onClick={() => setMode("friend-mode")}>👥 Play with Friend</button>
+                <button className="mode-btn computer" onClick={() => setMode("computer-mode")}>🤖 Play with Computer</button>
+              </div>
             </div>
-        )
-        :
-        (
+          )
+          :
+
+          (
             <>
-              <div className="gameboard">
-                {cells.map((cell, index) => (
-                  // Rendering Cell component
-                  <Cell
-                    id={index}
-                    go={go}
-                    setGo={setGo}
-                    cells={cells}
-                    setCells={setCells}
-                    cell={cell}
-                    isWin={isWin}
-                    ishighlighted={winCases.includes(index)}
-                    key={index} />
-                ))}
-              </div>
-              <div className="boardinfo">
-                <span>{(go === "cross" ? "❌" : "⭕️")}</span> turn
-              </div>
+              {
+
+                isWin ?
+                  (
+                    <div className="popup">
+                      <h2 className={resultClass()}>
+                        {isWin === "cross" && "❌ Cross Wins!"}
+                        {isWin === "circle" && "⭕️ Circle Wins!"}
+                        {isWin === "draw" && "🤝 It's a Draw!"}
+                      </h2>
+                      <button className="play-again-btn" onClick={handlePlayAgain}>Play Again</button>
+                    </div>
+                  )
+                  :
+                  (
+                    <>
+                      <div className="gameboard">
+                        {cells.map((cell, index) => (
+                          // Rendering Cell component
+                          <Cell
+                            id={index}
+                            go={go}
+                            setGo={setGo}
+                            cells={cells}
+                            setCells={setCells}
+                            cell={cell}
+                            isWin={isWin}
+                            ishighlighted={winCases.includes(index)}
+                            key={index} />
+                        ))}
+                      </div>
+                      <div className="boardinfo">
+                        <span>{(go === "cross" ? "❌" : "⭕️")}</span> turn
+                      </div>
+                    </>
+                  )
+              }
             </>
-        )
+          )
       }
     </div>
   );
